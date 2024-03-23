9bdf562130bf6fd3a2a84c14761f810eb0ce807f
Jace <1195684841@qq.com>
2024-03-23 21:25:42 +0800

    fix: 第二小结jsx

---
+++ v02/vite-runner/App.jsx
@@ -0,0 +1,15 @@
+/*
+ * @desc:
+ * @Author: Jace
+ * @Date: 2024-03-23 18:42:15
+ * @LastEditors: Jace
+ * @LastEditTime: 2024-03-23 21:04:53
+ */
+import React from './core/React.js'
+// const App = React.createElement('div', { id: 'app' }, 'app')
+const App = <div>app react</div>
+function AppTest () {
+  return <div>app AppTest</div>
+}
+console.log(AppTest)
+export default App
---
+++ v02/vite-runner/main.jsx
@@ -0,0 +1,13 @@
+/*
+ * @desc:
+ * @Author: Jace
+ * @Date: 2024-03-23 20:57:14
+ * @LastEditors: Jace
+ * @LastEditTime: 2024-03-23 21:09:12
+ */
+import React from "./core/React.js"
+import ReactDOM from "./core/reactDom.js"
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.querySelector('#root')).render(App)
