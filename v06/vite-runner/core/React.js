8a6a65b9b98df470a0c3d7e1b49487412bfd3799
Jace <1195684841@qq.com>
2024-03-25 21:58:25 +0800

    fix: function component

---
+++ v06/vite-runner/core/React.js
@@ -0,0 +1,154 @@
+import { ViteRuntime } from "vite/runtime"
+
+/*
+ * @desc:
+ * @Author: Jace
+ * @Date: 2024-03-23 17:18:52
+ * @LastEditors: Jace
+ * @LastEditTime: 2024-03-25 21:50:44
+ */
+function createTextNode (text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+function createElement (type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => {
+        const isTextNode = typeof child === 'string' || typeof child === 'number'
+        return isTextNode ? createTextNode(child) : child
+      })
+    }
+  }
+}
+
+
+
+function render (el, container) {
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  }
+  root = nextWorkOfUnit
+}
+let root = null;
+let nextWorkOfUnit = null
+function workLoop (deadline) {
+
+  let shouldYield = false
+  while (!shouldYield && nextWorkOfUnit) {
+    // run task
+
+    nextWorkOfUnit = performworkOfUnit(nextWorkOfUnit)
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  if (!nextWorkOfUnit && root) {
+    //
+    commitRoot()
+  }
+  requestIdleCallback(workLoop)
+}
+
+function commitRoot () {
+  commitWork(root.child);
+  root = null
+}
+function commitWork (fiber) {
+  if (!fiber) return
+  let fiberParent = fiber.parent
+  while (!fiberParent.dom) {
+    fiberParent = fiberParent.parent
+  }
+
+  if (fiber.dom) {
+    fiberParent.dom.append(fiber.dom)
+  }
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+
+
+function createDom (type) {
+  return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type)
+}
+
+function updateProps (dom, props) {
+  Object.keys(props).forEach((key) => {
+    if (key !== 'children') {
+      dom[key] = props[key]
+    }
+  })
+}
+
+function initChildren (fiber, children) {
+  // const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+
+
+function performworkOfUnit (fiber) {
+  const isFunctionComponent = typeof fiber.type === 'function'
+
+  if (!isFunctionComponent) {
+    // 1.创建dom
+    if (!fiber.dom) {
+      const dom = (fiber.dom = createDom(fiber.type))
+      // 2.处理props
+      updateProps(dom, fiber.props)
+    }
+  }
+  // 3.转换链表 设置好指针
+  const children = isFunctionComponent ? [fiber.type(fiber.props)] : fiber.props.children
+  initChildren(fiber, children)
+
+  // 4. 返回下一个要执行的任务
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  // while
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
+  }
+}
+
+requestIdleCallback(workLoop)
+
+
+
+const React = {
+  render,
+  createElement
+}
+
+export default React
